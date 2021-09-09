86b4b17c3b2c926186bb0e1ea0fb98308c8c65fc
Santiago Delmonte <delmontesantiago@gmail.com>
2021-09-09 20:16:54 -0300

    Ultimas modificaciones en componentes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Juego from './components/Juego';
 class App extends Component {
   render() {
     return (
-      <div className="layout">
+      <div>
         <Juego />
       </div>
     );
--- src/components/Juego.jsx
+++ src/components/Juego.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import data from './data.json'
+import Historial from "./Historial";
 import Opciones from "./Opciones";
+
+const eleccionesPrevias = [];
 
 class Juego extends Component {
 
@@ -14,22 +17,60 @@ class Juego extends Component {
 
   }
 
-  // this.setState({ pokemonList: pokemones, loading: false})
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contador !== this.state.contador) {
+      eleccionesPrevias.push(this.state.seleccionAnterior);
+    }
+  }
 
-  handleClick = () => {
-    alert("hice click!")
+  handleClick = (e) => {
+    const id = e.target.id;
+    if (this.state.contador > 7){
+      alert("Fin!")
+    } else if (id === "A" && this.state.seleccionAnterior !== "A") {
+      this.setState({ 
+        contador: this.state.contador + 1, 
+        seleccionAnterior: "A" 
+      })
+    } else if (id === "A" && this.state.seleccionAnterior === "A"){
+      this.setState({ 
+        contador: this.state.contador + 2
+      })
+    } else if (id === "B" && this.state.seleccionAnterior === "B"){
+      this.setState({ 
+        contador: this.state.contador + 3,
+        seleccionAnterior: "B"
+      })
+    } else if (id === "B"){
+      this.setState({ 
+        contador: this.state.contador + 2,
+        seleccionAnterior: "B"
+      })
+    } 
+
   }
 
   render() {
     return (
       <>
-      <div className="historia">
-        <h1>{data[this.state.contador].historia}</h1>
-
+      <div className="layout">
+        <h1 className="historia">{data[this.state.contador].historia}</h1>
+        <Opciones 
+            optionSelect={this.handleClick} 
+            opcionA={data[this.state.contador].opciones.a} 
+            opcionB={data[this.state.contador].opciones.b} 
+          />
+        <Historial
+          seleccionPrevia={this.state.seleccionAnterior}
+          historial={eleccionesPrevias.map(
+            (opcionAnterior, index) => (
+              <li key={index}>{opcionAnterior}</li>
+            ),
+            data[this.state.contador].id
+          )}
+        />
       </div>
-      <div>
-          <Opciones optionSelect={this.handleClick} evento={this.state.contador} />
-      </div>
+      
       </>
     )
   }
